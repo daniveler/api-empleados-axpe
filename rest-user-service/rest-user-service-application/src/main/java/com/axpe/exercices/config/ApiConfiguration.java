c7a9365ad71f9e58568f217429b5be701d0f6448
daniveler <daniveler@usal.es>
2022-09-16 00:04:50 +0200

    Basic autenthication implemented

--- rest-user-service/rest-user-service-application/src/main/java/com/axpe/exercices/config/ApiConfiguration.java
+++ rest-user-service/rest-user-service-application/src/main/java/com/axpe/exercices/config/ApiConfiguration.java
@@ -4,8 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.axpe.exercices.service.EmployeeService;
@@ -14,6 +18,28 @@ import com.axpe.exercices.service.EmployeeService;
 @EnableJpaAuditing
 public class ApiConfiguration
 {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+		http.csrf().disable()
+			.authorizeRequests().anyRequest().authenticated()
+			.and()
+			.httpBasic();
+		
+		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() 
+	{
+		UserDetails user = User.withUsername("daniveler")
+				.password("{noop}daniaxpe")
+				.roles("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer()
 	{
